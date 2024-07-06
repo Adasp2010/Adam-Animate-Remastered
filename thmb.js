632b84fb728c04aed8dc65e4e78febd59b4c75c1
Adam The GoAnimater <155333922+Adasp2010@users.noreply.github.com>
2024-07-06 17:42:10 -0400

    Add files via upload

--- thmb.js
+++ thmb.js
@@ -1,8 +1,13 @@
-const thumbUrl = process.env.THUMB_BASE_URL;
-const get = require('../request/get');
-
+const starter = require('./main');
 module.exports = function (req, res, url) {
-	if (req.method != 'GET' || !url.path.startsWith('/stock_thumbs')) return;
-	get(thumbUrl + url.path.substr(url.path.lastIndexOf('/'))).then(v => res.end(v));
+	if (req.method != 'GET' || !url.path.startsWith('/starter_thumbs')) return;
+	starter.thumb(url.path.substr(url.path.lastIndexOf('/') + 1))
+		.then(v => {
+			res.setHeader('Content-Type', 'image/png');
+			res.statusCode = 200; res.end(v);
+		})
+		.catch(() => {
+			res.statusCode = 400; res.end();
+		});
 	return true;
 }
